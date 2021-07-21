19dafc56b64d1cfc1f511231e4e3f6727f21913e
lverghese <loganverghese@gmail.com>
2021-07-20 23:10:23 -0400

    listing issues for exact repo and redirects

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -61,12 +61,14 @@ var getUserRepos = function(user) {
         
 
             //create a container for each repo
-            var repoEl = document.createElement("div");
+            var repoEl = document.createElement("a");
             repoEl.classList = "list-item flex-row justify-space-between align-center";
+            repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
             //create a span element to hold repository name
             var titleEl = document.createElement("span");
             titleEl.textContent = repoName;
+            
 
             //append to container
             repoEl.appendChild(titleEl);
